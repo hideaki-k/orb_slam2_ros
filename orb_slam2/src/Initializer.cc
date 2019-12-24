303530f5573848e1224e1c65e79217519e50dfe7
hideaki-k <h.kinoshita@kimura-lab.net>
2019-12-24 09:59:10 +0900

    0.40 mososi

--- orb_slam2/src/Initializer.cc
+++ orb_slam2/src/Initializer.cc
@@ -112,7 +112,7 @@ bool Initializer::Initialize(const Frame &CurrentFrame, const vector<int> &vMatc
     float RH = SH/(SH+SF);
 
     // Try to reconstruct from homography or fundamental depending on the ratio (0.40-0.45)
-    if(RH>0.01)
+    if(RH>0.40)
         return ReconstructH(vbMatchesInliersH,H,mK,R21,t21,vP3D,vbTriangulated,1.0,50);
     else //if(pF_HF>0.6)
         return ReconstructF(vbMatchesInliersF,F,mK,R21,t21,vP3D,vbTriangulated,1.0,50);
